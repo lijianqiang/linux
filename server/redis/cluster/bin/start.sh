8a97b5042f054327b9898582804ea8987c794adc
lijianqiang <silenceljq@gmail.com>
2016-10-15 20:36:37 +0800

    Update start.sh

--- server/redis/cluster/bin/start.sh
+++ server/redis/cluster/bin/start.sh
@@ -5,12 +5,12 @@ rm -rf *.rdb
 find ./ -name "nodes-700*" -exec rm -rf "{}" \;
 find ./ -name "redis.pid" -exec rm -rf "{}" \;
 echo 'starting redis-cluster ...'
-cd /data/redis_cluster/redis-3.2.4/
-src/redis-server /data/redis_cluster/nodes/7000/redis.conf
-src/redis-server /data/redis_cluster/nodes/7001/redis.conf
-src/redis-server /data/redis_cluster/nodes/7002/redis.conf
-src/redis-server /data/redis_cluster/nodes/7003/redis.conf
-src/redis-server /data/redis_cluster/nodes/7004/redis.conf
-src/redis-server /data/redis_cluster/nodes/7005/redis.conf
+cd /data/redis_cluster/
+redis-3.2.4/src/redis-server /data/redis_cluster/nodes/7000/redis.conf
+redis-3.2.4/src/redis-server /data/redis_cluster/nodes/7001/redis.conf
+redis-3.2.4/src/redis-server /data/redis_cluster/nodes/7002/redis.conf
+redis-3.2.4/src/redis-server /data/redis_cluster/nodes/7003/redis.conf
+redis-3.2.4/src/redis-server /data/redis_cluster/nodes/7004/redis.conf
+redis-3.2.4/src/redis-server /data/redis_cluster/nodes/7005/redis.conf
 ps -ef | grep redis-server
-src/redis-trib.rb create --replicas 1 120.77.16.193:7000 120.77.16.193:7001 120.77.16.193:7002 120.77.16.193:7003 120.77.16.193:7004 120.77.16.193:7005
+redis-3.2.4/src/redis-trib.rb create --replicas 1 120.77.16.193:7000 120.77.16.193:7001 120.77.16.193:7002 120.77.16.193:7003 120.77.16.193:7004 120.77.16.193:7005
