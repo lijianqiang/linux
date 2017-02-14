fdb70b4f68858c95188881beba86bc25405df003
李健强 <li.jianqiangfz@zte.com.cn>
2017-02-14 19:29:07 +0800

    update bash, add path

--- server/java/tomcat/bin/jmaven_8080.sh
+++ server/java/tomcat/bin/jmaven_8080.sh
@@ -8,4 +8,5 @@ export SERVER_CONF="-config conf/jmaven_8080.xml"
 
 JAVA_OPTS='-server -Xrs -Xmx512m -Xms126m -Xmn512m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:SurvivorRatio=8 -Xverify:none -Djava.net.preferIPv4Stack=true -Dfile.encoding=GBK -Duser.timezone=GMT+08 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false'
 
+cd `dirname $0` #进入这个目录(切换当前工作目录)
 . ./execute.sh
