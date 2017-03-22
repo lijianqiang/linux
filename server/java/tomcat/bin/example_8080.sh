16cc7fb929d3c52289b448f9b07a4d0a64330140
lijianqiang <silenceljq@gmail.com>
2017-03-22 09:17:18 +0800

    utf8

--- server/java/tomcat/bin/example_8080.sh
+++ server/java/tomcat/bin/example_8080.sh
@@ -6,7 +6,7 @@ export CATALINA_OUT=/home/lijianqiang/logs/tomcat/example_8081.out
 export LOGGING_CONFIG="-Djava.util.logging.config.file=../conf/example_8080.properties"
 export SERVER_CONF="-config conf/example_8080.xml"
 
-JAVA_OPTS='-server -Xrs -Xmx512m -Xms126m -Xmn512m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:SurvivorRatio=8 -Xverify:none -Djava.net.preferIPv4Stack=true -Dfile.encoding=GBK -Duser.timezone=GMT+08 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false'
+JAVA_OPTS='-server -Xrs -Xmx512m -Xms126m -Xmn512m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:SurvivorRatio=8 -Xverify:none -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 -Duser.timezone=GMT+08 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false'
 
 cd `dirname $0` #进入这个目录(切换当前工作目录)
 . ./execute.sh
